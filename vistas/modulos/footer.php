b6b71cb1956bf4f1d57e7a80ca680bb48b05e6d1
Jocelyn Martinez <ijrmartinez142@gmail.com>
2023-08-22 18:42:15 -0500

    actualizacion

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2023 <a href="https://www.tutorialesatualcance.com" target="_blank">Ferreteria Miguel "El Foco"</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
